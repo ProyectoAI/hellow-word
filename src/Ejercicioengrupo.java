69f4c4aff214598c20b4a34f28a3803605384f4c
L1E2 <L1E2@DESKTOP-TN9H3P2>
2022-02-10 12:36:09 -0500

    fdhjgj

---
+++ src/Ejercicioengrupo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ejercicioengrupo {
+    public static void main(String []args){
+        
+        Scanner sc = new Scanner(System.in);
+        int edad;
+        int cedula[];
+        String nombre, apellido;
+        char genero;
+        long altura, peso;
+        System.out.println("Ingrese sus nombres: ");
+        nombre=sc.nextLine();
+        System.out.println("Ingrese sus apellidos: ");
+        apellido=sc.nextLine();
+        while()
+            https://github.com/ProyectoAI/hellow-word.git
+    }
+    
+}
---
+++ src/ejercicio.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ejercicio {
+    public static void main(String []args){
+        
+        Scanner sc = new Scanner(System.in);
+        int edad;
+        int cedula[];
+        String nombre, apellido, genero;
+        long altura, peso;
+        System.out.println("Ingrese sus nombres: ");
+        nombre=sc.nextLine();
+    }
+}
